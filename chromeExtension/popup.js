f4617d57b9f1f3f75c8da7b9bc2fbabb28bec362
Caleb Behunin <calebrobot88@gmail.com>
2019-04-03 15:15:16 -0700

    I FIGURED OUT HOW TO UPDATE THE EXTENSION

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -9,21 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Modify Forms and Such here in the popup and reload it somehow ?
   let categories = fetchCategories();
-  
-  for(let i = 0; i < categories.len; i++) {
+
+  function updatePopup(categories) {
+     for(let i = 0; i < categories.len; i++) {
     let form = document.createElement("td");
     form.appendChild(categories[i]);
     location.appendChild(form)
   }
+}
 
-  /*
-  <script>
-var para = document.createElement("p");
-var node = document.createTextNode("This is new.");
-para.appendChild(node);
-var element = document.getElementById("div1");
-element.appendChild(para);
-</script>
+ /*
+ var para = document.createElement("td");
+ var node = document.createTextNode("This is new.");
+ para.appendChild(node);
+ var element = document.getElementById("categories");
+ element.appendChild(para);
 */
 
   //
