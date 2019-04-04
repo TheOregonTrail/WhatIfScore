ba6dcbaf347a0498ff3d36a3bc055b8b3ce07baa
luigishat <calebrobot88@gmail.com>
2019-04-03 22:25:35 -0700

    The Freaking Categorie Name Appender Works YESSSSSSSS

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -7,27 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
-  // Modify Forms and Such here in the popup and reload it somehow ?
-  let categories = fetchCategories();
+// Fix Bug: Forms are Highlighed Red probably because of css
 
-  function updatePopup(categories) {
-     for(let i = 0; i < categories.len; i++) {
-    let form = document.createElement("td");
-    form.appendChild(categories[i]);
-    location.appendChild(form)
-  }
+// IT FREAKING WORKS 
+// Appends categorie to Chrome Extension !
+  let categories = [ "Exam", "Homework", "Lab" ]
+  function updatePopup(categorie) {
+    // Make first <td> and append categorie's name to it
+    let categorieName = document.createElement("td");
+    let node = document.createTextNode(categorie.toString());
+
+// All neccessary to make a form in the second <td> tag
+    let weight = document.createElement("td");
+      let form = document.createElement("form");
+        let input = document.createElement("input");
+    input.type = "number";
+    input.value = "33.3";
+    let row = document.createElement("tr");
+
+// Do all the dirty html appending
+    categorieName.appendChild(node);
+    let tableId = document.getElementById("categories");
+    tableId.appendChild(categorieName);
+    tableId.appendChild(weight.appendChild(form).appendChild(input));
+    tableId.appendChild(row);
 }
 
- /*
- var para = document.createElement("td");
- var node = document.createTextNode("This is new.");
- para.appendChild(node);
- var element = document.getElementById("categories");
- element.appendChild(para);
-*/
+updatePopup(categories[0]);
 
   //
-  let submit = document.getElementsById("submit");
+  let submit = document.getElementById("submit");
   submit.addEventListener("clicked", function(){
     console.log("Submit button works");
   })
